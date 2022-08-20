3aca779e89b66b7e812eee8f14cffcebcab87ac7
lschacon1988 <lschacon1988@gmail.com>
2022-08-20 16:28:29 -0500

    update

--- client/src/views/Pokemons.jsx
+++ client/src/views/Pokemons.jsx
@@ -1,19 +1,18 @@
-
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Pokemon from "../componet/Pokemon";
 import {
   filterCreate,
-  filterType,  
+  filterType,
   ordenBy,
   ordenByPower,
 } from "../store/action";
 import s from "../style/card.module.css";
-import load from '../style/loading.module.css'
+import load from "../style/loading.module.css";
 import Paginado from "./Paginado";
 import Nav from "./Nav";
 import React from "react";
-
+import Container from "@mui/material/core/Container";
 
 export default function Pokemons(props) {
   const {
@@ -22,9 +21,8 @@ export default function Pokemons(props) {
 
   const dispatch = useDispatch();
 
- 
-  const loading = useSelector(state=> state.app.loading)
-  const [order, setOrder] = useState(""); 
+  const loading = useSelector((state) => state.app.loading);
+  const [order, setOrder] = useState("");
   const pokemons = useSelector((state) => state.pokemonReducers.pokemons);
   // PAGINADO
   const [currentPage, setCurrentPage] = useState(1);
@@ -32,23 +30,22 @@ export default function Pokemons(props) {
   const indexOfLastPage = currentPage * limitPage;
   const indexOffirstpage = indexOfLastPage - limitPage;
   const currentPokemos = pokemons.slice(indexOffirstpage, indexOfLastPage);
-  const next = (e) =>{
-      e.preventDefault()
-      if(currentPokemos.length<9){
-          return
-      }else{
-          setCurrentPage(currentPage+1)
-      }
-  }
-  const prev = (e) =>{
-    e.preventDefault()
-    if(currentPage<=1){
-        setCurrentPage(1)
-    }else{
-        setCurrentPage(currentPage-1)
+  const next = (e) => {
+    e.preventDefault();
+    if (currentPokemos.length < 9) {
+      return;
+    } else {
+      setCurrentPage(currentPage + 1);
     }
-}
-
+  };
+  const prev = (e) => {
+    e.preventDefault();
+    if (currentPage <= 1) {
+      setCurrentPage(1);
+    } else {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   const paginado = (pag) => {
     setCurrentPage(pag);
@@ -76,31 +73,46 @@ export default function Pokemons(props) {
     dispatch(ordenByPower(e.target.value));
     setOrder(e.target.value);
   }
-  console.log(currentPokemos)
+  console.log(currentPokemos);
   return (
-    <div className={s.container_home}>
-     <Nav  navigate={handleFilterType} page={setCurrentPage} handle={handleFilterCreate} handleOrderAlf={handleOderByAlf} halndelPow={handleOderByPow}/>     
+    <Container>
+      <div className={s.container_home}>
+        <Nav
+          navigate={handleFilterType}
+          page={setCurrentPage}
+          handle={handleFilterCreate}
+          handleOrderAlf={handleOderByAlf}
+          halndelPow={handleOderByPow}
+        />
 
-      <div className={s.contenedor}>
-      { loading ? 
-        <img className={load.loading} alt='' src={'http://dribbble.s3.amazonaws.com/users/121337/screenshots/1024835/loading2.gif'}/>:
-        currentPokemos?.map((pokemon) => (
-          <Pokemon          
-            pokemon={pokemon}
-            // key={pokemon.speed}
-            navigate={handleNavigate}
-          />
-        ))
-      }
+        <div className={s.contenedor}>
+          {loading ? (
+            <img
+              className={load.loading}
+              alt=""
+              src={
+                "http://dribbble.s3.amazonaws.com/users/121337/screenshots/1024835/loading2.gif"
+              }
+            />
+          ) : (
+            currentPokemos?.map((pokemon) => (
+              <Pokemon
+                pokemon={pokemon}
+                // key={pokemon.speed}
+                navigate={handleNavigate}
+              />
+            ))
+          )}
+        </div>
+        <Paginado
+          currentPokemos={currentPokemos.length}
+          next={next}
+          prev={prev}
+          pokemons={pokemons?.length}
+          limitPage={limitPage}
+          paginado={paginado}
+        />
       </div>
-      <Paginado
-      currentPokemos={currentPokemos.length}
-      next={next}
-      prev={prev}        
-        pokemons={pokemons?.length}
-        limitPage={limitPage}
-        paginado={paginado}
-      />
-    </div>
+    </Container>
   );
 }

0a35b93c0e8775ed2aea0611eacd21f9822612f2
lschacon1988 <lschacon1988@gmail.com>
2022-08-20 14:31:11 -0500

    update

--- api/src/db.js
+++ api/src/db.js
@@ -1,26 +1,19 @@
+//@ts-check
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { MONGOOSE_URI } = process.env;
 
-require('dotenv').config()
-const mongoose = require('mongoose')
-const {MONGOOSE_URI}=process.env
+const uri = `${MONGOOSE_URI}`;
 
+mongoose.connect(uri).catch((err) => {
+  console.log("ERROR AL CONECTAR", err);
+});
 
+const db = mongoose.connection;
 
-const uri = MONGOOSE_URI
-
-
-
-
-
-mongoose.connect(uri)
-.catch(err=>{
-console.log('ERROR AL CONECTAR', err)
-})
-
-const db = mongoose.connection
-
-db.on('open', _=>{
-    console.log('conectado a ', uri)
-})
-db.on('error',err=>{
-    console.log('error en db', err)
-})
+db.on("open", (_) => {
+  console.log("conectado a ", uri);
+});
+db.on("error", (err) => {
+  console.log("error en db", err);
+});

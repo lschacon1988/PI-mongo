5353403a030f0cb4dba5afbc801df742c49df137
lschacon1988 <lschacon1988@gmail.com>
2022-08-21 15:42:46 -0500

    Update Home.jsx

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,25 +1,24 @@
 import { TabContainer } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import s from "../style/landy.module.css"
+import s from "../style/landy.module.css";
 
 export default function Home() {
   return (
     <TabContainer>
-
-    <main className={s.home}>
-      <h1>Pokemons</h1>
-      <section>
-        <Link to="/pokemons" >
-          <a className={s.btn_neon}>
-            <span className={s.span1}></span>
-            <span className={s.span2}></span>
-            <span className={s.span3}></span>
-            <span className={s.span4}></span>
-            Vamos a ver los pokemon
-          </a>
-        </Link>
-      </section>
-    </main>
+      <main className={s.home}>
+        <h1>Pokemons</h1>
+        <section>
+          <Link to="/pokemons">
+            <a className={s.btn_neon}>
+              <span className={s.span1}></span>
+              <span className={s.span2}></span>
+              <span className={s.span3}></span>
+              <span className={s.span4}></span>
+              Vamos a ver los pokemon
+            </a>
+          </Link>
+        </section>
+      </main>
     </TabContainer>
   );
 }

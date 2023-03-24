e0ba253007e247c09c8d33f255d9ff90fb1f6155
lschacon1988 <lschacon1988@gmail.com>
2023-03-24 08:46:26 -0500

    base url

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
- import store from "./store";
+import store from "./store";
 import axios from "axios";
 import dotenv from 'dotenv'
 dotenv.config()

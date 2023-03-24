83815713cda0cc98034be46c20c640cb153c2bc4
lschacon1988 <lschacon1988@gmail.com>
2023-03-24 08:27:14 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import axios from "axios";
 import dotenv from 'dotenv'
 dotenv.config()
 
-axios.defaults.baseURL =  process.env.REACT_APP_API || 'https://pi-pokeapp-mongo.herokuapp.com/'
+axios.defaults.baseURL =  process.env.REACT_APP_API || 'https://pi-mongo-production.up.railway.app/'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
